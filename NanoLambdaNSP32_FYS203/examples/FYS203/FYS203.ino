9b47dd23f794cd529f23bf76de0c6de6ccc7eb09
Lars Øvergård <lars.overgard@hotmail.com>
2022-12-07 15:07:56 +0100

    docs: Added copyright

--- NanoLambdaNSP32_FYS203/examples/FYS203/FYS203.ino
+++ NanoLambdaNSP32_FYS203/examples/FYS203/FYS203.ino
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2019 nanoLambda, Inc.
+ * Copyright (C) 2022 Eik Lab, NMBU
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
